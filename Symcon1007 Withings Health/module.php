40705cc7b92ed8e36152aeb8eed55d3fb00e39d7
1007 <juergen.gerharz@inisnet.de>
2020-08-07 09:56:36 +0200

    Init

--- Symcon1007 Withings Health/module.php
+++ Symcon1007 Withings Health/module.php
@@ -1,22 +1,59 @@
 <?php
+
+
+//******************************************************************************
+//	Name		:	Withings Health Modul.php
+//	Aufruf		:	
+//	Info		:	
+//
+//******************************************************************************
+
 	class WithingsHealth extends IPSModule {
 
 		public function Create()
 		{
+			
+			$this->RegisterPropertyInteger("Intervall", 3600);
+			$this->RegisterTimer("WTH_UpdateTimer", 3600000, 'WTH_Update($_IPS["TARGET"]);');
+
 			//Never delete this line!
 			parent::Create();
+
 		}
 
 		public function Destroy()
 		{
+
+			$this->UnregisterTimer("WTH_UpdateTimer");
+
 			//Never delete this line!
 			parent::Destroy();
 		}
 
 		public function ApplyChanges()
 		{
+
+			  	//Timer stellen
+				  $interval = $this->ReadPropertyInteger("Intervall") ;
+				  $this->SetTimerInterval("WTH_UpdateTimer", $interval*1000);
+	  
+				  $this->SetStatus(102);
+
 			//Never delete this line!
 			parent::ApplyChanges();
 		}
+
+
+	   	//**************************************************************************
+		// manuelles Holen der Daten oder ueber Timer
+		//**************************************************************************
+		public function Update()
+			{
+		
+			$this->SendDebug(__FUNCTION__,"" ,0);
+		
+		
+		
+			}	
 
 	}
